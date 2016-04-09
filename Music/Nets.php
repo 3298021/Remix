28bfbffd7b9d04e19a1d75f64252a8f914b249f1
3298021 <149385359@qq.com>
2016-04-10 00:18:48 +0800

    增加一些功能
    无版权停播，增加一个备用的国内网易返回博客地址*(可以理解为代理)，增加代理服务器模块

--- Music/Nets.php
+++ Music/Nets.php
@@ -16,7 +16,8 @@ class Remix_Music_Nets implements Remix_Music_Interface
     public function song($id)
     {
         $url = 'http://music.163.com/api/song/detail/?id=' . $id . '&ids=%5B' . $id . '%5D';
-        $result = $this->http($url);
+        //$url = 'http://www.yqssgm.com/MusicAPI.php?bgmusic_id=' .$id;
+        $result = $this->curl_get($url);
 
         if (is_null($result)) {
             return;
@@ -54,7 +55,7 @@ class Remix_Music_Nets implements Remix_Music_Interface
     public function album($id)
     {
         $url = 'http://music.163.com/api/album/' . $id;
-        $result = $this->http($url);
+        $result = $this->curl_get($url);
 
         if (is_null($result)) {
             return;
@@ -79,7 +80,7 @@ class Remix_Music_Nets implements Remix_Music_Interface
     public function collect($id)
     {
         $url = 'http://music.163.com/api/playlist/detail?id=' . $id;
-        $result = $this->http($url);
+        $result = $this->curl_get($url);
 
         if (is_null($result)) {
             return;
@@ -103,11 +104,12 @@ class Remix_Music_Nets implements Remix_Music_Interface
      */
     public function http($url)
     {
+			  
         $client = Typecho_Http_Client::get();
 
         $client->setHeader('Cookie', 'appver=2.0.2')
         ->setHeader('Referer', 'http://music.163.com')
-        ->setTimeout(20)
+        ->setTimeout(5)
         ->send($url);
 
         if (200 === $client->getResponseStatus()) {
@@ -120,6 +122,37 @@ class Remix_Music_Nets implements Remix_Music_Interface
         }
         return;
     }
+    /**
+     * 自定义的curl
+     */
+    public function curl_get($url)
+    {
+        $refer = "http://music.163.com/";
+        $header[] = "Cookie: " . "appver=2.0.2";
+        $ch = curl_init();
+        
+        $porxy = Typecho_Widget::widget('Widget_Options')->plugin('Remix')->proxyAddNet;
+        $port = Typecho_Widget::widget('Widget_Options')->plugin('Remix')->proxyPortNet;
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_REFERER, $refer);
+			  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        
+        if ($porxy == '0.0.0.0') {
+        } else {
+			      curl_setopt($ch, CURLOPT_PROXY, $porxy);//代理服务器地址
+			      curl_setopt($ch, CURLOPT_PROXYPORT, $port);//代理服务器端口
+			      //curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
+			      curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式 
+			  }
+			  
+        $output = Json::decode(curl_exec($ch), true);
+        curl_close($ch);
+        return $output;
+    }
 
     /**
      * 解析歌曲
@@ -128,7 +161,7 @@ class Remix_Music_Nets implements Remix_Music_Interface
      */
     protected function parse($track)
     {
-        $src = str_replace('http://m', 'http://p', $track['mp3Url']);
+    	  $src = str_replace('http://m', 'http://p', $track['mp3Url']);
         $authors = array();
 
         foreach ($track['artists'] as $artist) {
@@ -136,7 +169,12 @@ class Remix_Music_Nets implements Remix_Music_Interface
         }
 
         $author = implode(',', $authors);
-
+        
+        if ( empty($src) )
+        {
+        	  $track['name'] = '(无版权停播) '.$track['name'];
+        }
+        
         $song = array(
             'id'     => $track['id'],
             'title'  => $track['name'],
